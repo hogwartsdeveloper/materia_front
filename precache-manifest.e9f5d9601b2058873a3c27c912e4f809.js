ba8db57785fff18c704227408c5b31a523112c70
Zhannur <jannuraidynuly@gmail.com>
2021-12-26 05:11:18 +0500

    Updates

---
+++ precache-manifest.e9f5d9601b2058873a3c27c912e4f809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aecf9c6b439feac5a3b9",
+    "url": "/materia_front/static/css/main.1c804fff.chunk.css"
+  },
+  {
+    "revision": "aecf9c6b439feac5a3b9",
+    "url": "/materia_front/static/js/main.aecf9c6b.chunk.js"
+  },
+  {
+    "revision": "c6cb031f49408df77e02",
+    "url": "/materia_front/static/js/1.c6cb031f.chunk.js"
+  },
+  {
+    "revision": "0530c882663975f94c9b",
+    "url": "/materia_front/static/js/2.0530c882.chunk.js"
+  },
+  {
+    "revision": "468d97fe3e062f62d71a",
+    "url": "/materia_front/static/js/runtime~main.468d97fe.js"
+  },
+  {
+    "revision": "a4812c112ebb8fe8289cbab1aebe95f0",
+    "url": "/materia_front/index.html"
+  }
+];
